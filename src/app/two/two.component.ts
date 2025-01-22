a339d1e88db83a9dda02f233738a99dbdc47848d
Filip Strandberg <filip.strandberg@hyph.com>
2025-01-22 10:43:05 +0100

    remove unused imports

--- src/app/two/two.component.ts
+++ src/app/two/two.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-two',
-  imports: [MatTab, MatTabGroup, RouterModule, MatButton],
+  imports: [RouterModule, MatButton],
   templateUrl: './two.component.html',
   styleUrl: './two.component.scss'
 })
